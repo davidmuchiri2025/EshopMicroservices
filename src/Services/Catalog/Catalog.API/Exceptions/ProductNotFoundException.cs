3c77559988b12fa31254a7bde251d49aba9f8cee
david <david@DESKTOP-CS4FH3J>
2025-02-23 19:30:41 +0300

    update

---
+++ src/Services/Catalog/Catalog.API/Exceptions/ProductNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿namespace Catalog.API.Exceptions;
+
+    public class ProductNotFoundException :Exception
+    {
+
+        public ProductNotFoundException(Guid id) :base("Product not found")
+        {
+            
+        }
+    }
+
